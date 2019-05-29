23f1550bc9c44746ff7a45da9cfacbaf057e2a80
Hoang Pham <huynhnguyenv2@gmail.com>
2019-05-29 14:29:41 +0700

    set up public static path

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.static('public'));
 
 
 app.get('/', function(request, respond){
