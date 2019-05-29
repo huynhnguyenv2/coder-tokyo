0605dff2d6de073688bebb82129037a9832f18f3
Hoang Pham <huynhnguyenv2@gmail.com>
2019-05-29 09:27:38 +0700

    intial Coder.Tokyo project

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+db.defaults({
+	users: []
+}).write()
+
+app.get('/', function(request, respond){
+	respond.render('index', {
+		name: 'Hoang'
+	})
+});
+
+app.get('/users', function(req,res){
+	res.render('users/index', {
+		users: db.get('users').value()
+	});
+});
+
+app.get('/users/search', function(req, res){
+	var q = req.query.q;
+	var users = db.get('users').value();
+	var matchedUsers = users.filter(function(user){
+		 return user.name.indexOf(q) !== -1
+		});
+	
+	res.render('users/index', {
+		users: matchedUsers
+	})
+});
+
+app.get('/users/create', function(req, res){
+	res.render('users/create')
+});
+
+app.get('/users/:id',function(req,res){
+	var id = parseInt(req.params.id);
+  //console.log(id)
+	var user = db.get('users').find({id : id});
+  //console.log(user);
+	res.render('users/view',{
+		user: user
+	})
+})
+app.post('/users/create', function(req, res){
+	db.get('users').push(req.body).write();
+	res.redirect('/users');
+});
+app.listen(3000, function(){
+	console.log('Sever is listening ' + port)
+});
