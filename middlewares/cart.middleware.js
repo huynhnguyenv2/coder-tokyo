51635a9a71209f0d89ae18c9b24a989ba49e451c
Hoang Pham <huynhnguyenv2@gmail.com>
2019-06-10 23:29:34 +0700

    update a little thing

--- middlewares/cart.middleware.js
+++ middlewares/cart.middleware.js
@@ -3,28 +3,30 @@ const Session = require("../models/session.model");
 const Product = require("../models/product.model");
 module.exports = async function(req, res, next){
   try {
-    var sessionId = req.signedCookies.sessionId;
+    let sessionId = req.signedCookies.sessionId;
     let item = await Session.findOne({
       ident: sessionId
     });
 
     if(item.cart !== undefined){
       let t = item.cart;
-      var total = Object.values(t).reduce( (a,b) => a + parseInt(b), 0);
-      var temp = [] ;
-      Object.keys(t).forEach(function(i){
-        Product.findById(i).then(function(x){ 
-          temp.push(x)
-        })
+      let total = Object.values(t).reduce( (a,b) => a + parseInt(b), 0);
+      let temp = [] ;
+      Object.keys(t).forEach(async function(i){
+        const x = await Product.findById(i);
+        temp.push(x);  
       });
-
-      var objCart = {
+      let objCart = {
         sum: total,
         details: temp 
       }
+      
       res.locals.cart = objCart;
     }
-    else res.locals.cart = false;
+    else res.locals.cart = {
+      sum: 0,
+      details: []
+    };
     next();
   } catch (error) {   
     next(error);
